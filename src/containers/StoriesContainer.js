f707372a37aed43b2a4260ba6c11e2737e2c40a1
Daniel Samuel <dansam5000@gmail.com>
2021-12-20 16:51:27 +0100

    feat: Add story container component

---
+++ src/containers/StoriesContainer.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import Story from '../components/Story';
+import { getStoryIds } from '../services/hnApi';
+
+const StoriesContainer = () => {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then((data) => setStoryIds(data));
+  }, []);
+  return storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />);
+};
+
+export default StoriesContainer;
